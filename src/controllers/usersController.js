565060eb9a59190524cae4dc245ca7520e0c3f9e
xjmx315 <xjmx315@gmail.com>
2025-05-02 10:53:38 +0900

    login과 getToken 분리

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -26,20 +26,26 @@ const login = async (req, res) => {
     if (!password || !name){
         return res.status(400).json({message: "id와 비밀번호는 필수 항목입니다"});
     }
-    const token = await usersService.login(password, name);
-    if (token){
+    const isCorrect = await usersService.login(password, name);
+    if (isCorrect){
+        const token = usersService.getToken(name, isCorrect);
         return res.status(200).json({message: "로그인 성공", token});
     }
     else {
         return res.status(202).json({message: "아이디 또는 비밀번호가 올바르지 않습니다"});
     }
-
-    return res.status(200).json({message: "로그인 성공", token});
 };
 
 const deleteUser = (req, res) => {
-    const {password, name} = req.body;
+    const {password} = req.body;
     usersService.deleteUser(password, name);
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: '인증 토큰이 필요합니다.' });
+    }
+
+    const token = authHeader.split(' ')[1];
     
 };
 
--- src/services/usersService.js
+++ src/services/usersService.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const JWT_KEY = dotenv.env.JWT_KEY || 'noJWT_KEY';
 
 const getIdByName = async (name) => {
     return await users.getIdByName(name);
@@ -20,6 +21,9 @@ const addUser = async (userData) => {
 };
 
 const login = async (password, name) => {
+    //비밀번호와 이름을 검증해서 맞으면 아이디를 반환. 
+    //아니면 false 반환
+
     //유저 검색
     const ids = await users.getIdByName(name);
     if (ids.length === 0){
@@ -39,16 +43,35 @@ const login = async (password, name) => {
         return false;
     }
 
-    //토큰 발급
-    const token = jwt.sign(
-        {userName: name, userId: id},
-        dotenv.env.JWT_KEY,
-        {expiresIn: "1h"}
-    );
-
-    return token;
+    return id;
 };
 
+const getToken = (userName, userId) => {
+    //name과 id 정보가 담긴 토큰을 생성
+    //토큰 발급
+    const token = jwt.sign(
+        {userName: userName, userId: userId},
+        JWT_KEY,
+        {expiresIn: "1h"}
+    );
+};
+
+const authUser = (token) => {
+    //유저 인증: 토큰을 받아서 유효하다면 유저 아이디를 반환. 
+    //유효하지 않으면 -1을 반환. 
+    if (!token) {
+        return -1;
+    }
+
+    try{
+        const decoded = jwt.verify(token, JWT_KEY);
+        return decoded;
+    }
+    catch (e) {
+        console.log("token error: ", e);
+        return -1;
+    }
+};
 
 const deleteUser = (password, name) => {
     
