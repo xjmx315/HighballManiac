596b04d1f2d3161ff678d47675d762b7b813e874
xjmx315 <xjmx315@gmail.com>
2025-06-10 21:10:59 +0900

    server.js app.js 분리

---
+++ src/app.js
@@ -0,0 +1,30 @@
+//app.js
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+import logger from './middlewares/logger.js';
+import apiRouter from './routes/apiRouter.js';
+
+// 환경 변수 로드
+dotenv.config();
+
+// express obj 생성
+const app = express();
+
+//미들웨어 플로우
+if (process.env.DEV){
+    app.use(cors());
+}
+else{
+    app.use(cors({
+        origin: process.env.REACT_ORIGIN
+    }));
+}
+
+app.use(express.json());
+app.use(logger);
+
+app.use('/api', apiRouter);
+
+export default app;
--- src/server.js
+++ src/server.js
@@ -1,40 +1,13 @@
-// 패키지 로드
+//server.js
+
+import app from './app.js';
 import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
 
-import logger from './middlewares/logger.js';
-import apiRouter from './routes/apiRouter.js';
-
-// 환경 변수 로드
+//포트 설정
 dotenv.config();
-
-// express obj 생성
-const app = express();
-
-// 포트 설정
 const PORT = process.env.PORT || 3000;
 
-//미들웨어 플로우
-if (process.env.DEV){
-    app.use(cors());
-}
-else{
-    app.use(cors({
-        origin: process.env.REACT_ORIGIN
-    }));
-}
-
-app.use(express.json());
-app.use(logger);
-
-app.get('/', (req, res) => {
-    res.send('hi~');
-});
-
-app.use('/api', apiRouter);
-
 // 서버 실행
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
