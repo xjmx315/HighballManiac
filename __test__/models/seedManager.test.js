5f197d4f3e623ab31efae53cd73b261d9223e6a7
xjmx315 <xjmx315@gmail.com>
2025-06-20 23:11:50 +0900

    스키마 구조 수정

--- __test__/models/seedManager.test.js
+++ __test__/models/seedManager.test.js
@@ -1,6 +1,8 @@
 import seedManager from "../../src/models/seedManager.js";
 import db from "../../src/models/db.js";
 
+/*db 초기화 작업을 진행하는 다른 테스트와 같이 진행시 테스트가 실패하는 경우가 있으니 단독으로 테스트 해주세요. */
+
 describe('seedManager Test', () => {
     test('init DB', async () => {
         await seedManager.initDB();
--- src/models/sql/schema.sql
+++ src/models/sql/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     email VARCHAR(127), 
-    password VARCHAR(225) NOT NULL, 
+    password VARCHAR(255) NOT NULL, 
     name VARCHAR(127) NOT NULL UNIQUE, 
     created_at DATETIME NOT NULL
 );
@@ -9,8 +9,15 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS items (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(127) UNIQUE, 
-    description VARCHAR(225), 
-    image VARCHAR(225)
+    description VARCHAR(255), 
+    image VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS ingredients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(127) UNIQUE,
+    description VARCHAR(255),
+    image VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS recipes (
@@ -18,18 +25,31 @@ CREATE TABLE IF NOT EXISTS recipes (
     user_id INT,
     name VARCHAR(127), 
     description VARCHAR(511),
-    image VARCHAR(225),
+    recipe VARCHAR(1023),
+    image VARCHAR(255),
     alcohol_percentage FLOAT,
     created_at DATETIME,
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+
 CREATE INDEX idx_recipes_alcohol_percentage ON recipes(alcohol_percentage);
+
+CREATE TABLE IF EXISTS comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT,
+    user_id INT,
+    content VARCHAR(511),
+    created_at DATETIME,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS tags (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(63) UNIQUE
+    name VARCHAR(31) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS recipe_tags (
@@ -41,21 +61,29 @@ CREATE TABLE IF NOT EXISTS recipe_tags (
     FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS steps (
-    recipe_id INT,
-    description VARCHAR(511),
-    image VARCHAR(225),
-    step_number INT,
-
-    PRIMARY KEY (recipe_id, step_number),
-    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
-);
-
 CREATE TABLE IF NOT EXISTS recipes_items (
     recipe_id INT, 
     item_id INT,
 
     PRIMARY KEY (recipe_id, item_id),
     FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS recipes_ingredients (
+    recipe_id INT, 
+    ingredient_id INT,
+
+    PRIMARY KEY (recipe_id, ingredient_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS items_ingredients (
+    ingredient_id INT, 
+    item_id INT,
+
+    PRIMARY KEY (ingredient_id, item_id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE,
     FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
 );
