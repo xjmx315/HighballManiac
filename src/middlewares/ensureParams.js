ac0dfe8e233ac6f9692ae8f8b41d427d93b6e5a6
xjmx315 <xjmx315@gmail.com>
2025-09-29 17:35:00 +0900

    [refactor] 에러 응답 구조 수정

--- src/middlewares/ensureParams.js
+++ src/middlewares/ensureParams.js
@@ -1,6 +1,6 @@
 //ensureParams.js
 
-import CommonResponse from "../prototype/commonResponse.js";
+import { ValidationError } from "../errors/CommonError.js";
 
 function _checkObj(obj, arr) {
     for (const element of arr) { 
@@ -47,31 +47,31 @@ function ensureParams () {
                 //쿼리 검사
                 const missingQuery = _checkObj(req.query, required.query);
                 if (missingQuery) {
-                    return res.status(400).json(new CommonResponse(false, 400, `필수 쿼리가 누락되었습니다. (${missingQuery})`));
+                    return next(new ValidationError(`필수 쿼리가 누락되었습니다. (${missingQuery})`));
                 }
 
                 //body 필드 검사
                 const missingField = _checkObj(req.body, required.body);
                 if (missingField) {
-                    return res.status(400).json(new CommonResponse(false, 400, `필수 필드가 누락되었습니다. (${missingField})`));
+                    return next(new ValidationError(`필수 필드가 누락되었습니다. (${missingField})`));
                 }
 
                 //param 필드 검사
                 const missingParam = _checkObj(req.params, required.params);
                 if (missingParam) {
-                    return res.status(400).json(new CommonResponse(false, 400, `필수 파라미터가 누락되었습니다. (${missingParam})`));
+                    return next(new ValidationError(`필수 파라미터가 누락되었습니다. (${missingParam})`));
                 }
                 
                 //숫자 자료형 검사
                 for (const element of required.shouldNumberList) {
                     if (element in req.query && !_isNumber(req.query[element])) {
-                        return res.status(400).json(new CommonResponse(false, 400, `쿼리 형식이 잘못되었습니다. (${element})`));
+                        return next(new ValidationError(`쿼리 형식이 잘못되었습니다. (${element})`));
                     }
                     if (element in req.body && !_isNumber(req.body[element])) {
-                        return res.status(400).json(new CommonResponse(false, 400, `필드 형식이 잘못되었습니다. (${element})`));
+                        return next(new ValidationError(`필드드 형식이 잘못되었습니다. (${element})`));
                     }
                     if (element in req.params && !_isNumber(req.params[element])) {
-                        return res.status(400).json(new CommonResponse(false, 400, `파라미터 형식이 잘못되었습니다. (${element})`));
+                        return next(new ValidationError(`파라미터 형식이 잘못되었습니다. (${element})`));
                     }
                 };
 
@@ -79,7 +79,6 @@ function ensureParams () {
             };
         }
     };
-
     return middleware;
 };
 
